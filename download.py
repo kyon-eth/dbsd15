37277cdb760fde3fa3b1a1ff2eef659d7b313034
Kyon <kyonosuke.eth@gmail.com>
2023-01-18 23:04:52 +0500

    yolo

--- download.py
+++ download.py
@@ -3,24 +3,10 @@
 
 import os
 import torch
-import requests
 from diffusers import StableDiffusionPipeline
 from diffusers.models import AutoencoderKL
 from huggingface_hub import HfFolder
 from transformers import pipeline
-
-
-def download_file(url, local_filename):
-    # NOTE the stream=True parameter below
-    with requests.get(url, stream=True) as r:
-        r.raise_for_status()
-        with open(local_filename, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=8192): 
-                # If you have chunk encoded response uncomment if
-                # and set chunk_size parameter to None.
-                #if chunk: 
-                f.write(chunk)
-    return local_filename
 
 # Download the weights
 def download_model():
@@ -28,13 +14,9 @@ def download_model():
     TOKEN = os.getenv('HF_AUTH_TOKEN')
     HfFolder.save_token(TOKEN)
     
-    print("downloading model from civitai")
-    
-    model = download_file("https://civitai.com/api/download/models/4007?type=Model&format=PickleTensor", "models/model.ckpt")
-    
     print("downloading model: stabilityai/sd-vae-ft-mse & runwayml/stable-diffusion-v1-5...")
-    
-    # model = "runwayml/stable-diffusion-v1-5"
+
+    model = "runwayml/stable-diffusion-v1-5"
     vae = AutoencoderKL.from_pretrained("stabilityai/sd-vae-ft-mse")
     pipe = StableDiffusionPipeline.from_pretrained(model, vae=vae)
 
